9ad115a35e9b2945824f20ef662d3289842cf1e2
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-11 12:56:49 -0800

    Add files via upload

---
+++ Player/Views/Layout.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Player.Views
+{
+    /// <summary>
+    /// Interaction logic for Layout.xaml
+    /// </summary>
+    public partial class Layout : Page
+    {
+        public Layout()
+        {
+            InitializeComponent();
+        }
+    }
+}
